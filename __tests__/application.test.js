bb4733f7aa16488691ceea6540ac352d1d88718a
roman-usov <66104246+roman-usov@users.noreply.github.com>
2023-08-10 15:26:36 +0400

    updated and ran tests

--- __tests__/application.test.js
+++ __tests__/application.test.js
@@ -21,28 +21,28 @@ beforeEach(async () => {
 test('i18n', async () => {
   expect(screen.getByRole('button', { name: /0 clicks/i })).toBeInTheDocument();
   expect(screen.getByRole('button', { name: /reset/i })).toBeInTheDocument();
-  await userEvent.click(screen.getByRole('button', { name: /русский/i }));
+  await userEvent.default.click(screen.getByRole('button', { name: /русский/i }));
   expect(await screen.findByRole('button', { name: /0 кликов/i })).toBeInTheDocument();
   expect(screen.getByRole('button', { name: /сбросить/i })).toBeInTheDocument();
-  await userEvent.click(screen.getByRole('button', { name: /english/i }));
+  await userEvent.default.click(screen.getByRole('button', { name: /english/i }));
   expect(await screen.findByRole('button', { name: /0 clicks/i })).toBeInTheDocument();
   expect(screen.getByRole('button', { name: /reset/i })).toBeInTheDocument();
 });
 
 test('plurals', async () => {
-  await userEvent.click(screen.getByRole('button', { name: /0 clicks/i }));
+  await userEvent.default.click(screen.getByRole('button', { name: /0 clicks/i }));
   expect(screen.getByRole('button', { name: /1 click/i })).toBeInTheDocument();
-  await userEvent.click(screen.getByRole('button', { name: /1 click/i }));
+  await userEvent.default.click(screen.getByRole('button', { name: /1 click/i }));
   expect(screen.getByRole('button', { name: /2 clicks/i })).toBeInTheDocument();
-  await userEvent.click(screen.getByRole('button', { name: /русский/i }));
+  await userEvent.default.click(screen.getByRole('button', { name: /русский/i }));
   expect(await screen.findByRole('button', { name: /2 клика/i })).toBeInTheDocument();
-  await userEvent.click(screen.getByRole('button', { name: /2 клика/i }));
-  await userEvent.click(screen.getByRole('button', { name: /3 клика/i }));
-  await userEvent.click(screen.getByRole('button', { name: /4 клика/i }));
-  await userEvent.click(screen.getByRole('button', { name: /5 кликов/i }));
-  await userEvent.click(screen.getByRole('button', { name: /сбросить/i }));
-  await userEvent.click(screen.getByRole('button', { name: /0 кликов/i }));
+  await userEvent.default.click(screen.getByRole('button', { name: /2 клика/i }));
+  await userEvent.default.click(screen.getByRole('button', { name: /3 клика/i }));
+  await userEvent.default.click(screen.getByRole('button', { name: /4 клика/i }));
+  await userEvent.default.click(screen.getByRole('button', { name: /5 кликов/i }));
+  await userEvent.default.click(screen.getByRole('button', { name: /сбросить/i }));
+  await userEvent.default.click(screen.getByRole('button', { name: /0 кликов/i }));
   expect(screen.getByRole('button', { name: /1 клик/i })).toBeInTheDocument();
-  await userEvent.click(screen.getByRole('button', { name: /english/i }));
+  await userEvent.default.click(screen.getByRole('button', { name: /english/i }));
   expect(await screen.findByRole('button', { name: /1 click/i })).toBeInTheDocument();
 });
